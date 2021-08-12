30a1f386865d16ac3db54a96c8eb89f623b818f6
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-12 15:04:47 -0400

    app.js update

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import AboutMe from './components/AboutMe';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="background" style={{ height: "100%" }}>
+      <Navbar />
+      <Header />
+      <AboutMe />
+      <Footer />
     </div>
+
+
   );
 }
 
