faf0ba2afb70cb92f4098cf80ae3f7be61456bef
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-12 16:32:25 -0400

    fixed app.js, decided against header

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,22 +1,39 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import AboutMe from './components/AboutMe';
-import Header from './components/Header';
+// import Header from './components/Header';
 import Footer from './components/Footer';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 
 function App() {
+
+  const [page, setPage] = useState("about");
+
+  const renderPage = (currentPage) => {
+    if (currentPage === "about") {
+      return <AboutMe />
+    } else if (currentPage === "contact") {
+      return <Contact />
+    } else if (currentPage === "portfolio") {
+      return <Portfolio />
+    }
+  }
+
+  const handlePageChange = (newPage) => setPage(newPage)
+
   return (
+
     <div id="background" style={{ height: "100%" }}>
-      <Navbar />
-      <Header />
-      <AboutMe />
-      <Footer />
+      <div id="overlay" style={{ height: "100%" }}>
+        <Navbar page={page} setPage={handlePageChange} />
+        {renderPage(page)}
+        <Footer />
+      </div>
     </div>
-
-
-  );
+  )
 }
 
 export default App;
--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -1,12 +1,14 @@
-import React from 'react';
-// import CSS?
+// import React from 'react';
+// // import CSS?
 
-export default function Header() {
-    return (
-        <div>
-            <h2>
-                Hello! Welcome to Eric Devine's portfolio. Also, change this text, sounds hammy.
-            </h2>
-        </div>
-    )
-}
+// export default function Header() {
+//     return (
+//         <div>
+//             <h2>
+//                 Hello! Welcome to Eric Devine's portfolio. Also, change this text, sounds hammy.
+//             </h2>
+//         </div>
+//     )
+// }
+
+// i think this is overall redundant, replace with navbar instead
